2b66b96166be45b9229c800afb65a4d27601afa5
Shaji-Mohammed <mshajisufiyan@gmail.com>
2023-10-15 22:17:12 -0300

    Created buy product function using TDD

--- Vending Machine/tests/test_vending_machine.py
+++ Vending Machine/tests/test_vending_machine.py
@@ -2,6 +2,7 @@
 The test module for Vending Machine
 """
 
+import pytest
 import vendingMachine
 
 inserted_coins_list = []
@@ -9,3 +10,11 @@ inserted_coins_list = []
 
 def test_insert_coin():
     assert vendingMachine.insert_coin(5, inserted_coins_list) == [5]
+
+
+def test_insert_invalid_coin():
+    with pytest.raises(ValueError):
+        assert vendingMachine.insert_coin(7, inserted_coins_list)
+
+def test_buy_product():
+    assert vendingMachine.buy_product("drink", 5) == (5 - 2.75)
