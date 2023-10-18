1543c23b389460668abe0fd32d9e2ad62446bb8f
Shaji-Mohammed <mshajisufiyan@gmail.com>
2023-10-18 13:24:17 -0300

    Updated the vm using more tests

--- Vending Machine/tests/test_vending_machine.py
+++ Vending Machine/tests/test_vending_machine.py
@@ -6,25 +6,47 @@ import pytest
 import vending_machine as vendingMachine
 from InsufficientFunds import InsufficientFunds
 
-inserted_coins_list = []
-
 
 def test_insert_coin():
-    assert vendingMachine.insert_coin(5, inserted_coins_list) == [5]
+    inserted_coins_list = []
+    assert vendingMachine.insert_coin_in_cents(5, inserted_coins_list) == 0.05
 
 
 def test_insert_invalid_coin():
+    inserted_coins_list = []
     with pytest.raises(ValueError):
-        assert vendingMachine.insert_coin(7, inserted_coins_list)
+        assert vendingMachine.insert_coin_in_cents(7, inserted_coins_list)
 
 
 def test_buy_product():
-    assert vendingMachine.buy_product("drink", 5) == (5 - 2.75)
+    inserted_coins_list = []
+    assert vendingMachine.buy_product("drink", 5) == [200, 25]
     with pytest.raises(InsufficientFunds):
         assert vendingMachine.buy_product("chips", 2)
 
 
 def test_return_change():
-    assert vendingMachine.return_change(5) == [200, 200, 100]
-    assert vendingMachine.return_change(10) == [200, 200, 200, 200, 200]
-    assert vendingMachine.return_change(2.75) == [200, 25, 25, 25]
+    inserted_coins_list = []
+    assert vendingMachine.return_change_in_cents(5) == [200, 200, 100]
+    assert vendingMachine.return_change_in_cents(10) == [200, 200, 200, 200, 200]
+    assert vendingMachine.return_change_in_cents(2.75) == [200, 25, 25, 25]
+
+def test_run_chips():
+    inserted_coins_list = []
+    balance = vendingMachine.insert_coin_in_cents(200, inserted_coins_list)
+    balance = vendingMachine.insert_coin_in_cents(100, inserted_coins_list)
+    assert vendingMachine.buy_product("chips", balance) == [25, 25, 25]
+
+def test_run_Drink():
+    inserted_coins_list = []
+    balance = vendingMachine.insert_coin_in_cents(200, inserted_coins_list)
+    balance = vendingMachine.insert_coin_in_cents(100, inserted_coins_list)
+    assert vendingMachine.buy_product("drink", balance) == [25]
+
+
+def test_run_Candy():
+    inserted_coins_list = []
+    balance = vendingMachine.insert_coin_in_cents(200, inserted_coins_list)
+    balance = vendingMachine.insert_coin_in_cents(25, inserted_coins_list)
+    balance = vendingMachine.insert_coin_in_cents(100, inserted_coins_list)
+    assert vendingMachine.buy_product("candy", balance) == [10]
--- Vending Machine/vending_machine.py
+++ Vending Machine/vending_machine.py
@@ -6,19 +6,32 @@ from InsufficientFunds import InsufficientFunds
 valid_coin_values = [5, 10, 25, 100, 200]
 reversed_list_coins = valid_coin_values
 reversed_list_coins.reverse()
-valid_products = {"drink": 2.75, "chips": 2.25, "candy": "3.15"}
+valid_products = {"drink": 2.75, "chips": 2.25, "candy": 3.15}
+current_balance = 0
 
 
-def insert_coin(coin, inserted_coins):
+def update_balance(current_balance, inserted_coins):
+    for i in range(0, len(inserted_coins)):
+        current_balance += inserted_coins[i]
+    return current_balance
+
+
+def convert_cents_to_dollar(coin):
+    return coin / 100
+
+
+def insert_coin_in_cents(coin, inserted_coins):
     """
     This is called when a user inserts coins in the vending machine to purchase an item
     :param coin: Coin inserted by the user
     :param inserted_coins: List for keep track of the coins inserted
     :return:
     """
+    global current_balance
     if coin in valid_coin_values:
-        inserted_coins.append(coin)
-        return inserted_coins
+        dollar = convert_cents_to_dollar(coin)
+        inserted_coins.append(dollar)
+        return update_balance(current_balance, inserted_coins)
     raise ValueError
 
 
@@ -31,18 +44,19 @@ def buy_product(product, balance):
     if product not in valid_products.keys():
         raise ValueError
     if balance >= valid_products.get(product):
-        return balance - valid_products.get(product)
+        balance = balance - valid_products.get(product)
+        return return_change_in_cents(balance)
     raise InsufficientFunds(f"Insufficient funds available to buy {product}")
 
 
-def return_change(balance):
+def return_change_in_cents(balance):
     """
     This method is called after the user purchases an item from the vending machine
     :param balance: The amount of money left after an item is purchased
-    :return: returns the balance in change (coins)
+    :return: returns the balance in change (coins/cents)
     """
     change_left = []
-    balance = balance * 100
+    balance = round(balance * 100)
     i = 0
     while balance > 0:
         while balance >= reversed_list_coins[i]:
